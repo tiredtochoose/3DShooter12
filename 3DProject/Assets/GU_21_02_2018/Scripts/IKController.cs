d98bf31f68f083bbb83503ff37b4610b3f6f5631
afatina <afatina@saber3d.ru>
2018-05-16 12:43:18 +0300

    GU_21_02_2018

---
+++ 3DProject/Assets/GU_21_02_2018/Scripts/IKController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+public class IKController : MonoBehaviour {
+    protected Animator animator;
+    public bool ikActive = false;
+    public Transform rightHandObj = null;
+    public Transform lookObj = null;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    //Вызывается при расчёте IK
+    void OnAnimatorIK()
+    {
+        if (animator)
+        {
+            //Если, мы включили IK, устанавливаем позицию и вращение
+            if (ikActive)
+            {
+                // Устанавливаем цель взгляда для головы
+                if (lookObj != null)
+                {
+                    animator.SetLookAtWeight(1);
+                    animator.SetLookAtPosition(lookObj.position);
+                }
+                // Устанавливаем цель для правой руки и выставляем её в позицию
+                if (rightHandObj != null)
+                {
+                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+                    animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandObj.position);
+                    animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandObj.rotation);
+                }
+            }
+            // Если IK неактивен, ставим позицию и вращение рук и головы в изначальное положение
+            else
+            {
+                animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0);
+                animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 0);
+                animator.SetLookAtWeight(0);
+            }
+        }
+    }
+}
